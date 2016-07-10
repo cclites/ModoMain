98654b98373b7ab9495de3ad6210005d3c46ac29
Chad Clites <cclites@sweeps-soft.com>
2016-07-09 20:38:05 -0500

    More updates to Stripe integration

--- public/js/ViewModels/Ledger.js
+++ public/js/ViewModels/Ledger.js
@@ -10,17 +10,24 @@ var buildLedgerView = function()
 
   
   str = "";
-  str += "  <div class='sectionHeader'>Balances:</div>\n";
-  str += "  <table class='balanceTable'>";
-  str += "    <tr>";
-  str += "      <th>USD: </th><td id='availableUsd" + id + "'>$" + myB.usd + "</td>"; 
-  str += "      <th>BTC: </th><td id='availableBtc" + id + "'>" + myB.btc + "</td>";
-  str += "      <th>REMAINING TRADES: </th><td id='availableTrades" + id + "'>" + myB.trades + "</td>";
+  str += "  <div class='sectionHeader'>Balances:</div>\n" +
+         "  <table class='balanceTable'>" +
+         "    <tr>" +
+         "      <th>USD: </th><td id='availableUsd" + id + "'>$" + myB.usd + "</td>" + 
+         "      <th>BTC: </th><td id='availableBtc" + id + "'>" + myB.btc + "</td>";
+         
+  if(model.paid == 1){
+  	str += "      <th>Subscribed:</th><td id='availableTrades" + id + "'>Yes</td>";
+  }else{
+  	str += "      <th>Subscribed:</th><td id='availableTrades" + id + "'>Yes</td>";
+  }
+  
+  //str += "      <th>REMAINING TRADES: </th><td id='availableTrades" + id + "'>" + myB.trades + "</td>";
   //str += "      <th>Purchase Price: </th><td id='ppp" + id + "'>$" + myB.ppp + "</td>";
   //str += "      <th>Sell Price: </th><td id='spp" + id + "'>$" + myB.spp + "</td>";
-  str += "    </tr>";
-  str += "  </table>";
-  str += "</div>";
+  str += "    </tr>" +
+         "  </table>" +
+         "</div>";
   
   return str;
 };
